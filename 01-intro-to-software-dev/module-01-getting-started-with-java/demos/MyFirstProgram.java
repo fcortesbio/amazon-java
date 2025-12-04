89930f27cee5e03bbab29b01c1e3501156123ce3
fcortesbio <fcortesbio@gmail.com>
2025-12-04 10:36:24 -0500

    move all demo files to dedicated directory

--- 01-intro-to-software-dev/module-01-getting-started-with-java/demos/MyFirstProgram.java
+++
@@ -1,7 +0,0 @@
-public class MyFirstProgram {
-    public static void main(String[] args){
-        System.out.println("I'm a Programmer!");
-        System.out.println("Let's change the code now!");
-        System.out.println("This is what I do!");
-    }
-}
